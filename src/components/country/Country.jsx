ac1b80094bfa52c84e0ad29606f4a55543651957
Abu Bakor Siddik (Shuvo) <141806136+Abs-shuvo@users.noreply.github.com>
2026-05-08 03:46:02 +0600

    added some files

---
+++ src/components/country/Country.jsx
@@ -0,0 +1,43 @@
+import React, { useState } from 'react';
+import './Country.css'
+
+const Country = ({country}) => {
+    const[visited,setVisited]=useState(false);
+    // console.log(country.area.area);
+
+    const handleVisited = () => {
+        //Using if else toggle
+        /* if(visited){
+            setVisited(false);
+        }else{
+            setVisited(true);
+        } */
+
+        //using ternary toggle
+        /* setVisited(visited? false : true); */
+
+        //Using bang sign toggle
+        setVisited(!visited)
+    }
+
+    return (
+        //Using AND Operator
+        //Using ternary operator
+        // <div className={`country border-lg text-center ${visited ? 'country-visited' : 'country'}`}>
+        <div className={`country ${visited && 'country-visited'}`}>
+
+            <img src={country.flags.flags.png} alt={country.flags.flags.alt} />
+            <h3>Name:{country.name.common} </h3>
+            <p>Population:{country.population.population} </p>
+            <p>Area: {country.area.area} & It is a {country.area.area> 2040 ? 'Big Country':"Small Country"}</p>
+            <button onClick={handleVisited}>{visited? 'Visited':'Not Visited'}</button>
+        </div>
+    );
+};
+
+export default Country;
+
+/**
+ * 1.Inline CSS(Style object)
+ * 2.
+ *  */ 
